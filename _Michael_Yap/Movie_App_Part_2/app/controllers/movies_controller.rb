cffcd88007aefb2d9f6b4966046dca8c0449a176
michaelryap <mchl.yp@gmail.com>
2014-07-30 08:54:42 -0400

    Coaxed edit functionality to work.

--- _Michael_Yap/Movie_App_Part_2/app/controllers/movies_controller.rb
+++ _Michael_Yap/Movie_App_Part_2/app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
   def index
   	@movies = Movie.all
+  	# Watch how you pluralizes instance variables.
+  	# The plural makes sense here because it contains all movies.
   end
 
   def edit
@@ -8,6 +10,16 @@ class MoviesController < ApplicationController
   end
 
   def update
+  	@movie = Movie.find(params[:id])
+
+  	if @movie.update(movie_params)
+  		redirect_to @movie
+  	else
+  		render "edit"
+  	end
+
+  	# The method update, is used when you want to update a record that already exists. It accepts a hash containing the attributes that you want to update.
+
   end
 
   def create
@@ -18,6 +30,9 @@ class MoviesController < ApplicationController
   	@movie = Movie.find(params[:id])
   end
 
-
+  private
+  	def movie_params
+  		params.require(:movie).permit(:title, :description)
+  	end
 
 end
--- _Michael_Yap/Movie_App_Part_2/app/models/movie.rb
+++ _Michael_Yap/Movie_App_Part_2/app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+
+	validates :title, presence: true, length: { minimum: 5 }
+	
 end
