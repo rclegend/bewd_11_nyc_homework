f41853827152340c3cf2f805d54598d3335b5298
michaelryap <mchl.yp@gmail.com>
2014-07-30 08:54:10 -0400

    Coaxed edit functionality to work.

--- _Michael_Yap/Movie_App_Part_2/app/controllers/movies_controller.rb
+++ _Michael_Yap/Movie_App_Part_2/app/controllers/movies_controller.rb
@@ -1,7 +1,23 @@
 class MoviesController < ApplicationController
   def index
-
   	@movies = Movie.all
-
   end
+
+  def edit
+  	@movie = Movie.find(params[:id])
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+
+  def show
+  	@movie = Movie.find(params[:id])
+  end
+
+
+
 end
