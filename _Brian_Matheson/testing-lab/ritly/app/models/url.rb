af8eb2bbff369bf32603460ea37c56888c72e19b
Brian Matheson <brian.k.matheson@gmail.com>
2014-08-04 15:50:30 -0400

    add tests and refactor url creation

--- _Brian_Matheson/testing-lab/ritly/app/models/url.rb
+++ _Brian_Matheson/testing-lab/ritly/app/models/url.rb
@@ -1,17 +1,10 @@
 class Url < ActiveRecord::Base
   validates :link, presence:true
 
-  def create (safe_url_params)
-    @url = Url.new safe_url_params
-    @url.hash_code = rand(1..1000000)
-
-    # For the bonus
-    # @url.hash_code = SecureRandom.urlsafe_base64(8)
-
-    @url.save
-
-    # Or create it in one shot by merging the random parameter into the safe params hash
-    # @url = Url.create safe_url_params.merge(hash_code: SecureRandom.urlsafe_base64(8))
+  def self.create(params)
+    url = Url.new params
+    url.hash_code = rand(1000000).to_s
+    url.save
+    return url
   end
-
 end
--- _Brian_Matheson/testing-lab/ritly/spec/models/url_spec.rb
+++ _Brian_Matheson/testing-lab/ritly/spec/models/url_spec.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Url, :type => :model do
-  context "creation" do
+  context "constructor" do
 
-    let!(:url) {Url.create()}
-    let!(:valid_url) {Url.create(link:'http://www.google.com')}
+    let(:valid_url) {Url.create(link:'http://www.google.com')}
 
-    it "creates a url" do
-      expect(Url.count).to eq 2
-    end
-    
-    it "requries a url as an argument" do
-      expect(url.link).to_not be_valid
+    it "creates a db entry" do
+      expect(valid_url.link).to be_present
+      expect(Url.count).to eq 1
     end
 
     it "accepts a url as an argument" do
       expect(valid_url.link).to be_present
+    end
+
+    it "validates the record" do
+      expect(valid_url).to be_valid
+    end
+
+    it "makes up a hash code" do
+      expect(valid_url.hash_code).to be_present
     end
   end
 end
